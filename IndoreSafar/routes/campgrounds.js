37773a4a7d25a0e08a193415f73ada95401da75a
Garima <garimaagrawal043.com>
2021-07-02 15:43:43 +0530

    init

--- IndoreSafar/routes/campgrounds.js
+++ IndoreSafar/routes/campgrounds.js
@@ -61,7 +61,7 @@ router.get("/campgrounds/:id",function(req,res){
 });
 
 // EDIT CAMPGROUND ROUTE
-router.get("/campgrounds/:id/edit", function(req, res){
+router.get("/campgrounds/:id/edit",checkCampgroundOwnership, function(req, res){
     Campground.findById(req.params.id, function(err, foundCampground){
         if(err){
             res.redirect("/campgrounds");
@@ -73,7 +73,7 @@ router.get("/campgrounds/:id/edit", function(req, res){
 });
 
 // UPDATE CAMPGROUND ROUTE
-router.put("/campgrounds/:id", function(req, res){
+router.put("/campgrounds/:id",checkCampgroundOwnership,function(req, res){
     // find and update the correct campground
     Campground.findByIdAndUpdate(req.params.id, req.body.campground, function(err, updatedCampground){
        if(err){
@@ -85,12 +85,47 @@ router.put("/campgrounds/:id", function(req, res){
     });
 });
 
+// DESTROY CAMPGROUND ROUTE
+router.delete("/campgrounds/:id",checkCampgroundOwnership, function(req, res){
+   Campground.findByIdAndRemove(req.params.id, function(err){
+      if(err){
+          res.redirect("/campgrounds");
+      } else {
+          res.redirect("/campgrounds");
+      }
+   });
+});
+
+
+//middleware
 function isLoggedIn(req, res, next){
     if(req.isAuthenticated()){
         return next();
     }
     // req.flash("error", "You need to be logged in to do that");
     res.redirect("/login");
+}
+
+function checkCampgroundOwnership(req, res, next) {
+ if(req.isAuthenticated()){
+        Campground.findById(req.params.id, function(err, foundCampground){
+           if(err){
+               //req.flash("error", "Campground not found");
+               res.redirect("back");
+           }  else {
+               // does user own the campground?
+            if(foundCampground.author.id.equals(req.user._id)) {
+                next();
+            } else {
+               // req.flash("error", "You don't have permission to do that");
+                res.redirect("back");
+            }
+           }
+        });
+    } else {
+        //req.flash("error", "You need to be logged in to do that");
+        res.redirect("back");
+    }
 }
 
 module.exports = router;
