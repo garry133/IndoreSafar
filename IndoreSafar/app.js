529d4b270db16cf673cefe540aa7e2c36a282bb2
Garima <garimaagrawal043.com>
2021-07-05 23:49:34 +0530

    add comment

--- IndoreSafar/app.js
+++ IndoreSafar/app.js
@@ -1,28 +1,28 @@
-var express     = require("express"),
-    app         = express(),
-    bodyParser  = require("body-parser"),
-    mongoose    =require("mongoose"),
-    flash       = require("connect-flash"),
-    passport    = require("passport"),
+var express = require("express"),
+    app = express(),
+    bodyParser = require("body-parser"),
+    mongoose = require("mongoose"),
+    flash = require("connect-flash"),
+    passport = require("passport"),
     LocalStrategy = require("passport-local"),
     methodOverride = require("method-override"),
-    Campground =require("./models/campground"),
+    Campground = require("./models/campground"),
     Comment = require("./models/comment"),
-    User        = require("./models/user"),
+    User = require("./models/user"),
     seedDB = require("./seeds");
 
 //requiring routes
-var commentRoutes    = require("./routes/comments"),
+var commentRoutes = require("./routes/comments"),
     campgroundRoutes = require("./routes/campgrounds"),
-    indexRoutes      = require("./routes/index");
+    indexRoutes = require("./routes/index");
 
-    //database
-mongoose.connect("mongodb://localhost:27017/yelp_camp_v7",{ useNewUrlParser: true , useUnifiedTopology: true })
-.then(()=> console.log("data base connection successful!!! "))
-.catch((err)=> console.log(err));
 
-app.use(bodyParser.urlencoded({extended: true}));
-app.set("view engine","ejs");
+mongoose.connect("mongodb://localhost:27017/yelp_camp_v7", { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => console.log("data base connection successful!!! "))
+    .catch((err) => console.log(err));
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.set("view engine", "ejs");
 app.use(express.static(__dirname + "/public"));
 app.use(methodOverride("_method"));
 app.use(flash());
@@ -40,17 +40,17 @@ passport.use(new LocalStrategy(User.authenticate()));
 passport.serializeUser(User.serializeUser());
 passport.deserializeUser(User.deserializeUser());
 
-app.use(function(req, res, next){
-   res.locals.currentUser = req.user;
-   res.locals.error = req.flash("error");
-   res.locals.success = req.flash("success");
-   next();
+app.use(function (req, res, next) {
+    res.locals.currentUser = req.user;
+    res.locals.error = req.flash("error");
+    res.locals.success = req.flash("success");
+    next();
 });
 
 app.use(indexRoutes);
 app.use(campgroundRoutes);
 app.use(commentRoutes);
 
-app.listen(3000,function(){
+app.listen(3000, function () {
     console.log("YelpCamp server is active!!");
 });
